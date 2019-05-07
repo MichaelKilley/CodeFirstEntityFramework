960dbbc9ea66d08363b17307cc8b38cc3f6b31c4
Michael Killey <michael.killey@gmail.com>
2019-05-07 18:01:04 +0100

    Add project files.

---
+++ CodeFirstEntityFramework/Models/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CodeFirstEntityFramework.Models
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+
+        [Required(ErrorMessage = "Firstname required")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "Enter Minimum Lenght 3 with a Maximum of 20 Characters")]
+     [RegularExpression(@"(\S)+", ErrorMessage = "cannot Start with an empty spaces")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "Lastname required")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "Enter Minimum Lenght 3 with a Maximum of 20 Characters")]
+        [RegularExpression(@"^[\w'\-,.][^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]]{2,}$", ErrorMessage = "cannot Start with an empty spaces")]
+
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string Mobile { get; set; }
+    }
+}
